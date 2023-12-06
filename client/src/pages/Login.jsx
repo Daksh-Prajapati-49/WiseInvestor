21111177250a12165210923749e1a1437088b5e3
Daksh Prajapati <89261276+Daksh-Prajapati-49@users.noreply.github.com>
2023-12-06 23:35:25 +0530

    Material Ui CSS added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,32 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const Login = ({ setSignin, setUser }) => {
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
     const navigate = useNavigate();
+
+
+
+    // Pasted as it is from material Ui 
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -15,12 +36,12 @@ const Login = ({ setSignin, setUser }) => {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                username : name,
+                username: name,
                 password,
                 email,
             }),
         });
-        
+
         console.log(response);
         if (response.status === 200) {
             const data = await response.json();
@@ -40,27 +61,68 @@ const Login = ({ setSignin, setUser }) => {
             <div>
                 <h1>Login</h1>
                 <form onSubmit={handleSubmit}>
-                    <input
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Name"
+                        multiline
+                        maxRows={4}
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <br />
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Email"
+                        multiline
+                        maxRows={4}
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <br />
+                    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Password"
+                        />
+                    </FormControl>
+
+                    {/* <input
                         type="text"
                         placeholder="Name"
                         value={name}
                         onChange={(e) => setName(e.target.value)}
-                    />
-                    <br/>
+                    /> */}
+                    {/* <br />
                     <input
                         type="password"
                         placeholder="Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                    <br/>
+                    <br />
                     <input
                         type="email"
                         placeholder="Email"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <br/>
+                    /> */}
+                    <br />
                     <button type="submit">Login</button>
                 </form>
             </div>
